d2a34d8d4394853c5cd0f9f2d48f549b10dd5e96
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-15 18:56:40 -0300

    feacture class para el layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,10 +4,13 @@ import { Outlet } from "react-router-dom"
 export const AuthLayout = () => {
     return (
         <>
-            <div>AuthLayout</div>
 
 
-            <Outlet />
+            <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:jus">
+                <div className="md:w-2/3 lg:1/2">
+                    <Outlet />
+                </div>
+            </main>
         </>
 
     )
